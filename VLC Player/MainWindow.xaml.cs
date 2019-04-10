6f7f821568ad62aee4de5fdd05a8491efff81642
Martin Hynek <W0olfikCZ@seznam.cz>
2019-04-10 11:47:57 +0200

    init

--- VLC Player/MainWindow.xaml.cs
+++ VLC Player/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,22 @@ namespace VLC_Player
         public MainWindow()
         {
             InitializeComponent();
+
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+
+            var vlcLibDirectory = new DirectoryInfo(System.IO.Path.Combine(currentDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
+
+            var options = new string[]
+            {
+                // VLC options can be given here. Please refer to the VLC command line documentation.
+            };
+
+            this.MyControl.SourceProvider.CreatePlayer(vlcLibDirectory, options);
+
+            // Load libvlc libraries and initializes stuff. It is important that the options (if you want to pass any) and lib directory are given before calling this method.
+            this.MyControl.SourceProvider.MediaPlayer.Play("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov");
         }
     }
 }
